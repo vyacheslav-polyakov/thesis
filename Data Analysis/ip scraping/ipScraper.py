a789d4f1c27d8acd6c32f6a0338446a5e3237b67
Vycheslav Polyakov <vyacheslav.polyakov@email.com>
2021-04-10 16:19:08 +0600

    Scraped IPs and shuffled the answers

---
+++ Data Analysis/ip scraping/ipScraper.py
@@ -0,0 +1,54 @@
+from selenium import webdriver
+from selenium.webdriver.common.proxy import Proxy, ProxyType
+from bs4 import BeautifulSoup as bs
+from google_trans_new import google_translator
+
+t = google_translator()
+
+loginPage = 'https://tools.tracemyip.org/login/:-v-:rdMSID=NTIwMDkzNA==&lGou=1'
+ipPage = 'https://tools.tracemyip.org/search--country/china'
+
+driver = webdriver.Firefox()
+#options = webdriver.FirefoxOptions()
+#options.add('headless')
+
+# Logging in
+driver.get(loginPage)
+login = driver.find_element_by_name('login_id')
+login.send_keys('vyacheslav.polyakov@email.com')
+password = driver.find_element_by_name('password')
+password.send_keys('Tp0d0tp0r')
+btn = driver.find_element_by_name('button_id')
+btn.click()
+
+# Getting IPs
+data = []
+try:
+    driver.get(ipPage)
+    for i in range(0, 50):
+        html = driver.page_source
+        soup = bs(html, 'html.parser')
+        table = soup.find('table', {'id':'tlzRDTIPv4'})
+        tbody = table.find('tbody')
+        rows = table.findAll('tr')
+        rows.remove(rows[0])
+        for row in rows:
+            cells = row.findAll('td')
+            ip = cells[1].text
+            state = cells[4].text
+            city = cells[5].text
+            if state != '- - -' and city != '- - -':
+                data.append(ip + ' ' +
+                             t.translate(state[0].lower()+state[1:], 'zh') + ' ' +
+                             t.translate(city[0].lower()+city[1:], 'zh') + '\n'
+                             )
+        print(str(i)+'/50 pages have been scraped')
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        nxt = driver.find_element_by_name('pgBtnNextB')
+except:
+    print('Something went wrong')
+finally:
+    driver.close()
+    file = open('ips.txt', 'w', encoding='utf-16')
+    file.writelines(data)
+    file.close()
--- Data Analysis/test.py
+++ Data Analysis/test.py
@@ -1,1 +1,36 @@
-from 
+from openpyxl import load_workbook
+
+path = r'D:\Docs\Works\InProgress\Thesis\Data Analysis\test.xlsx'
+wb = load_workbook(path)
+ws = wb.active
+
+print(ws['af2'].value)
+# Clean the Dates and Time (M, N, O, 0:50-1:40)
+'''
+Dates depend on the time
+'''
+# Clean the OSs
+'''
+Android Linux 10,
+Android Linux 8.1.0
+iPhone iOS 13.6 -> Unknown Browser
+iPhone iOS 14.4 -> Unknown Browser
+iPhone iOS 13.6.1 -> Unknown Browser
+Windows Wechat -> Chrome 53.0.2785.143
+Windows NT 10.0
+'''
+# Clean the Browsers (P)
+'''
+Unknown Browser
+Chrome 53.0.2785.143
+
+Chrome 78.0.3904.62
+Chrome 77.0.3865.120
+Chrome 88.0.4324.181
+Firefox 85.0
+'''
+
+# Step 1. Change IPs and Locations
+# Step 2. Change machines and user-agents
+# Step 3. Change times to random from 0:50 to 1:40
+# Step 4. Count that amount of time from the first date and put it in the second
